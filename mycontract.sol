b115f5dca134de307c2dd7e19c6ffbbd71f9fb87
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-02-23 16:34:38 +0400

    what is this?
    Here I play with solidity

---
+++ mycontract.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+    uint256 favoriteNumber = 5;
+    bool favoriteBool = true;
+    string favoriteString = "Adam Sandler";
+    int favoriteint = -5;
+    address favoriteAddress = 0xC6A00625D732F84A0D4a9110291892d9b7A7ce0c;
+    bytes32 favoriteBytes = "Cat";
+    //I don't know what bytes32 is. What I do know that you can write comment after double backslashes. 
+
+}
